07fb3fb5665abf2547c051cbf0342afe52f9508e
zcy <zurl@live.com>
2017-04-02 21:44:12 +0800

    refactor the dict structure

---
+++ AbstractIndexRunner.h
@@ -0,0 +1,12 @@
+//
+// Created by 张程易 on 2017/4/2.
+//
+
+#ifndef DB_ABSTRACTINDEXEXCUTER_H
+#define DB_ABSTRACTINDEXEXCUTER_H
+
+class AbstractIndexExecuter{
+
+};
+
+#endif //DB_ABSTRACTINDEXEXCUTER_H
---
+++ IndexRunner.h
@@ -0,0 +1,15 @@
+//
+// Created by 张程易 on 2017/4/2.
+//
+
+#ifndef DB_INDEXEXCUTER_H
+#define DB_INDEXEXCUTER_H
+
+#include "AbstractIndexExcuter.h"
+
+template<typename T>
+class IndexExcuter: public AbstractIndexExecuter{
+
+};
+
+#endif //DB_INDEXEXCUTER_H
